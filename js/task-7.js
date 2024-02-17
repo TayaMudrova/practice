70fd70e41424116b04e8d23082ce5c4ece85fda0
Vladyslav Mishchuk <v.mishuk2017@gmail.com>
2024-02-17 12:43:13 +0000

    I finished task 7

---
+++ js/task-7.js
@@ -0,0 +1,29 @@
+/**
+ *? Напиши скрипт для об'єкта user,
+ *? послідовно:
+ *? 1 додати поле mood зі значенням 'happy'
+ *? 2 замінить hobby на 'skydiving'
+ *? 3 замінить значення premium на false
+ *? 4 виводить вміст об'єкта users у форматі
+ *? ключ: значення використовуючи Object.keys() і for...of
+ */
+
+const user = {
+  name: "John",
+  age: 20,
+  hobby: "tennis",
+  premium: true,
+};
+
+user.mood = "happy";
+user.hobby = "skydiving";
+user.premium = false;
+
+const userKeys = Object.keys(user);
+
+const userKey = (user) => {
+  for (const key in user) {
+    console.log(`${key}: ${user[key]}`);
+  }
+};
+userKey(user);
