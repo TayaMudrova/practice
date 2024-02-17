d4622761016d1fc53eaad95aa7594486660e52b1
Taya Mudrova <mudrova.taisiia@gmail.com>
2024-02-17 15:07:26 +0200

    task-9

---
+++ js/task-9.js
@@ -0,0 +1,23 @@
+// Створіть об'єкт calculator із трьома методами
+// read(a, b) - приймає два аргументи та зберігає їх
+// як властивості об'єкта
+// sum() повертає суму збережених значень
+// multiply() перемножує збережені значення та повертає результат
+
+const calculator = {
+  read(a, b) {
+    this.value1 = a;
+    this.value2 = b;
+  },
+  sum() {
+    return this.value1 + this.value2;
+  },
+  multiply() {
+    return this.value1 * this.value2;
+  },
+};
+
+calculator.read(2, 3);
+console.log(calculator);
+console.log(calculator.sum());
+console.log(calculator.multiply());
